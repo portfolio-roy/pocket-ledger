266b176952ffddfadb3a80dd41fd26beebf5318e
Utsargo Roy <royutsargo@gmail.com>
2023-05-03 17:59:10 +0600

    Fix user model

--- app/models/User.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  validates :full_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+  end
